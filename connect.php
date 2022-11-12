ef086f4cb9c79d57fd2884aa5d012957ca290ffd
trgiahuy14 <trgiahuy14@gmail.com>
2022-11-12 17:14:24 +0700

    sualoiHinhAnh

---
+++ connect.php
@@ -0,0 +1,28 @@
+<?php
+  if (isset($_POST['chooseDate']) || isset($_POST['name']) || 
+  isset($_POST['phoneNumber']) || isset($_POST['email']) || isset($_POST['people']) || isset($_POST['note'])) {
+
+    $chooseDate = $_POST['chooseDate'];
+    $hours = $_POST['hours'];
+    $name = $_POST['name'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $email = $_POST['email'];
+    $people = $_POST['people'];
+    $note = $_POST['note'];
+
+    $conn = new mysqli('localhost','root','','testnhahangthangbomtestxampp');
+    if($conn->connect_error){
+      echo "$conn->connect_error";
+      die("Connection Failed : ". $conn->connect_error);
+	  } else {
+      $stmt = $conn->prepare("insert into datban(chooseDate, hours, name, phoneNumber, email, people, note) values(?, ?, ?, ?, ?, ?, ?)");
+      $stmt->bind_param("sssisis", $chooseDate, $hours, $name, $phoneNumber, $email, $people, $note);
+      $execval = $stmt->execute();
+      echo $execval;
+      echo "Registration successfully...";
+      $stmt->close();
+      $conn->close();
+      }
+      
+  }
+?>
