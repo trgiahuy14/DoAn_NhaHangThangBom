8ffde26ae609743269c3304b1845c79651c0fa87
trgiahuy14 <trgiahuy14@gmail.com>
2022-11-14 03:18:24 +0700

    chinhsuawebthongtinhom

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop() > 40){
+        if($(this).scrollTop() > 70){
             $("#topbtn").fadeIn();
         } else {
             $("#topbtn").fadeOut();
